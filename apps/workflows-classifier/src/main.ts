e0801421221ca86da72f9a5700050862aadaa79c
Administrator <saz99man.99@gmail.com>
2026-05-16 17:46:35 +0200

    added classifire and services for notification and mahfuz modified:   apps/workflows-classifier/src/main.ts modified:   apps/workflows-service/src/workflows/entities/workflow.entity.ts modified:   docker-compose.yml modified:   package-lock.json modified:   package.json modified:   tsconfig.json

--- apps/workflows-classifier/src/main.ts
+++ apps/workflows-classifier/src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   });
 
   await app.startAllMicroservices();
-  await app.listen(3002); // Use 3002 so it doesn't conflict with other apps running on 3000, 3001, or 3003
+  await app.listen(3000); // Use 3002 so it doesn't conflict with other apps running on 3000, 3001, or 3003
 }
 bootstrap();
